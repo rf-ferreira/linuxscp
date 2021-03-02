35b9f2a0176d60cdd99e0b6df406dad7c9b9f09b
Rafael <r4fael.s0uza@gmail.com>
2021-03-02 15:57:37 -0300

    add msgs de erro/sucesso

--- linuxscp.py
+++ linuxscp.py
@@ -38,47 +38,60 @@ class LinuxSCP(tk.Frame):
         self.button = tk.Button(text="Conectar", command=self.connect)
         self.button.grid(row=8, column=0)
 
-
     def connect(self):
         try:
             client.connect(self.server_ip_entry.get(), port=self.port_entry.get(), username=self.user_entry.get(), password=self.password_entry.get())
-            print('Conectado com sucesso')
             SCP()
-        except Exception as e:
-            print('Não foi possivel conectar ao servidor')
-            print(e)
+            root.withdraw()
+        except:
             tk.messagebox.showerror('Algo deu errado','Não foi possivel conectar ao servidor')
     
 class SCP:
     def __init__(self):
-       self.window = tk.Toplevel()
-       self.window.resizable(False, False)
-       self.window.title("Linux SCP")
-       self.widgets()
-
+        self.window = tk.Toplevel()
+        self.window.resizable(False, False)
+        self.window.title("Linux SCP")
+        self.window.protocol("WM_DELETE_WINDOW", self.close)
+        self.widgets()
+        
     def widgets(self):
-        print(server_ip)
+        self.lbl_server_directory = tk.Label(self.window, text="Diretorio do servidor")
+        self.lbl_server_directory.grid(row=0, column=0)
+        self.server_directory = tk.Entry(self.window)
+        self.server_directory.grid(row=1, column=0)
+
         self.btn_browse_files = tk.Button(self.window, text="Procurar arquivo", command=self.browse_files)
-        self.btn_browse_files.grid(row=0, column=0, sticky="EW")
+        self.btn_browse_files.grid(row=2, column=0, sticky="EW")
         self.btn_send = tk.Button(self.window, text="Enviar para servidor", command=self.send_to_server)
-        self.btn_send.grid(row=1, column=0)
+        self.btn_send.grid(row=3, column=0, sticky="EW")
+        
+        self.file_name = False
 
     def browse_files(self):
-        self.file = tk.filedialog.askopenfile(parent=root,mode='rb',title='Procurar arquivo')
+        self.file = tk.filedialog.askopenfile(parent=self.window,mode='rb',title='Procurar arquivo')
         if self.file:
             self.file_name = tk.Label(self.window, text=self.file.name)
-            self.file_name.grid(row=2, column=0)
+            self.file_name.grid(row=4, column=0)
 
     def send_to_server(self):
-        try:
+        if self.file_name and self.server_directory.get().strip() != '':
             ftp_client = client.open_sftp()
-            ftp_client.put(f'{self.file.name}', f'/root/{self.file.name.split("/")[-1]}')
+            ftp_client.put(f'{self.file.name}', f'{self.server_directory.get()}{self.file.name.split("/")[-1]}')
             ftp_client.close()
-            print('Arquivo enviado com sucesso')
-        except:
-            print('Nao foi possivel enviar o arquivo')
+            tk.messagebox.showinfo('Sucesso', 'Arquivo enviado', parent=self.window)
+        else:
+            tk.messagebox.showerror('Erro', 'Nao foi possivel enviar o arquivo', parent=self.window)
+        
+        if self.file_name:
+            self.file_name.destroy()
+            self.file_name = False
+        self.server_directory.delete(0, 'end')
+
+    def close(self):
+        root.quit()
 
 root = tk.Tk()
 root.title('Linux SCP')
+root.resizable(False, False)
 app = LinuxSCP(master=root)
 app.mainloop()
